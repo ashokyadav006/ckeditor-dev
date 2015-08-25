ddb937dcc797c03a5c4689a07ae5bb57bf012e29
ashokyadav006 <ashokyadav006@gmail.com>
2015-08-25 15:15:25 +0530

    Custom changes

---
+++ plugins/witexpiry/plugin.js
@@ -0,0 +1,15 @@
+﻿CKEDITOR.plugins.add('witexpiry', {
+    init: function (editor) {
+        var pluginName = 'witexpiry';
+        editor.addCommand(pluginName, {
+           exec: function (editor) {
+               $("#WitExpiryHiddenDOM").click();
+           }
+        });
+        editor.ui.addButton('Witexpiry', {
+            label: 'Witexpiry',
+            command: pluginName,
+            icon: this.path + 'wit_expiry.png'
+        });
+    }
+});
---
+++ plugins/wittyupload/plugin.js
@@ -0,0 +1,16 @@
+﻿CKEDITOR.plugins.add('wittyupload', {
+    init: function (editor) {
+        var pluginName = 'wittyupload';
+        editor.addCommand(pluginName, {
+           exec: function (editor) {
+               document.getElementById("insertImagePlugin").click();
+               console.log("Call insert image function !");
+           }
+        });
+        editor.ui.addButton('Wittyupload', {
+            label: 'Wittyupload',
+            command: pluginName,
+            icon: this.path + 'image.png'
+        });
+    }
+});
